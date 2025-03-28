ae8c1bbcdf93630c8de441108750049ffc3c114e
Michael Hansen <mike@rhasspy.org>
2025-03-28 09:25:23 -0500

    Bump to 2025.3.28

--- tests/test_intents.py
+++ tests/test_intents.py
@@ -10,3 +10,13 @@ def test_load_intents(language: str) -> None:
     """Test that we can load intents for every supported language."""
     lang_intents = get_intents(language)
     assert lang_intents
+
+
+# TODO: Need to add support for kw and sr-Latn
+# def test_language_scores() -> None:
+#     """Test that all supported languages are in language scores."""
+#     scores = get_language_scores()
+#     lang_families = {lang.split("-", maxsplit=1)[0] for lang in scores}
+
+#     for lang in get_languages():
+#         assert (lang in scores) or (lang in lang_families)
